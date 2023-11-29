badeac933ecc704455052a2ae21c8985f3fb6f09
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-11-29 09:16:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import pandas as pd
 import io
 
 st.title('Data visualization')
-st.header('Upload file')
+st.header('Upload data file')
 data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
 
 if data_file is not None:
- df = pd.read_csv(data_file)
+    df = pd.read_csv(data_file)
 
- st.header('Show data')
-st.dataframe(df)
+    st.header('Show data')
+    st.dataframe(df)
 
-st.header('Descriptive staticstics')
-st.table(df.describe())
+    st.header('Descriptive statistics')
+    st.table(df.describe())
 
-st.header('Show data information')
-buffer = io.stringIO()
-df.info(buf=buffer)
-st.text(buffer.getvalue())
+    st.header('Show data information')
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    st.text(buffer.getvalue())
