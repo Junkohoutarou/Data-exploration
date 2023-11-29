0d13387ee40d1987f3cf8d6598a985b3d8853946
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-11-29 09:20:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import io
-
+import matplotlib.pyplot as plt
 st.title('Data visualization')
 st.header('Upload data file')
 data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
@@ -20,3 +20,8 @@ if data_file is not None:
     buffer = io.StringIO()
     df.info(buf=buffer)
     st.text(buffer.getvalue())
+    st.header('Visualize each attribute')
+    for cel in_list(df.columns):
+        fig, ax = plt.subplots()
+    ax.hist(df[col], bins-20)
+    plt.xlabel(col)
