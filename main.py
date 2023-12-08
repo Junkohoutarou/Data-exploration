c52b73a777261cb07836eb95272b50aa139b596f
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 15:51:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 with open("styles.css") as f:
     custom_css = f.read()
-st.image("example_image.png", use_column_width=True)
+st.image("image.jpg", use_column_width=True)
 st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
 
 st.title('Data visualization')
